9a85f227453627121300a1f2f360b8daa858c390
tsuyoron <108647290+tsuyoron@users.noreply.github.com>
2022-08-21 06:17:36 +0000

    [Add]User#indexの左側を作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,24 @@
 class BooksController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.user_id = current_user.id
+    book.save
+    redirect_to book_path(book.id)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:name, :introduction)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,15 @@
 class UsersController < ApplicationController
   def index
-
+    @user = User.find_by(id: params[:id])
+    @book = Book.new
   end
+
+
 
   def show
 
   end
+
+
+
 end
